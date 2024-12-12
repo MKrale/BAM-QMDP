cac609a38cd1c4c107c8f7bf1b7d8a436c5f3d44
Jorrit de Boer <jorritdb@hotmail.com>
2024-12-12 14:40:19 +0100

    Add text episode recorder

--- AM_Gyms/PartialMeasurementLake.py
+++ AM_Gyms/PartialMeasurementLake.py
@@ -1,5 +1,6 @@
 from frozen_lake_v2 import FrozenLakeEnv_v2
 from ActiveMeasurementWrapper import ActiveMeasurementWrapper
+from TextEpisodeRecorder import TextEpisodeRecorder
 
 """"
 Test file for testing partial measurements. In this instance for Lake to only observe x or y 
@@ -28,7 +29,14 @@ def measurement_cost(measurement):
     return cost
 
 
-env = FrozenLakeEnv_v2(render_mode="human", map_name="4x4")
-env = ActiveMeasurementWrapper(
-    env, observation_function=obs_function, measurement_cost=measurement_cost
-)
+env = FrozenLakeEnv_v2(render_mode="ansi", map_name="4x4")
+env = TextEpisodeRecorder(env, folder="./episodes")
+for i in range(50):
+    termination, truncation = False, False
+    _ = env.reset(seed=123)
+    while not (termination or truncation):
+        obs, rew, termination, truncation, info = env.step(env.action_space.sample())
+
+# env = ActiveMeasurementWrapper(
+#     env, observation_function=obs_function, measurement_cost=measurement_cost
+# )
